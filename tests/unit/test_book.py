6234273f066e090a4ee56481f6d32464ffe7a7d6
chpancrate <chpancrate@gmail.com>
2023-09-28 09:54:32 +0200

    test design

--- tests/unit/test_book.py
+++ tests/unit/test_book.py
@@ -58,3 +58,25 @@ def test_book_page_with_wrong_competition(clubs_fix,
     response_data = response.data.decode()
 
     assert "<h1>Page not found</h1>" in response_data
+
+
+def test_book_page_with_full_data(clubs_fix,
+                                  competitions_fix,
+                                  client,
+                                  mocker):
+    """
+    GIVEN that the competition in the url does not exist
+    WHEN you arrive on the url
+    THEN the welcome page is displayed with an error message"
+    """
+    # mock of the club and competition lists
+    mocker.patch.object(server, "clubs", clubs_fix)
+    mocker.patch.object(server, "competitions", competitions_fix)
+
+    response = client.get('/book/Competition 1/Test Name 1')
+    response_data = response.data.decode()
+
+    assert "Competition 1" in response_data
+    assert "Places available: 25" in response_data
+    assert "Availables points: 15" in response_data
+    assert "<a href=\"{previous_url}\">" in response_data
